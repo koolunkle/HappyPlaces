d8172d450680314a4e04eff1618d97bd2347828e
koolunkle <a43361904@gmail.com>
2023-02-04 16:53:55 +0900

    덱스터 통해 권한 처리

--- app/src/main/java/com/udemy/happyplaces/AddHappyPlaceActivity.kt
+++ app/src/main/java/com/udemy/happyplaces/AddHappyPlaceActivity.kt
@@ -1,9 +1,20 @@
 package com.udemy.happyplaces
 
 import android.app.DatePickerDialog
+import android.content.ActivityNotFoundException
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.provider.Settings
 import android.view.View
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
+import com.karumi.dexter.Dexter
+import com.karumi.dexter.MultiplePermissionsReport
+import com.karumi.dexter.PermissionToken
+import com.karumi.dexter.listener.PermissionRequest
+import com.karumi.dexter.listener.multi.MultiplePermissionsListener
 import com.udemy.happyplaces.databinding.ActivityAddHappyPlaceBinding
 import java.text.SimpleDateFormat
 import java.util.*
@@ -36,6 +47,7 @@ class AddHappyPlaceActivity : AppCompatActivity(), View.OnClickListener {
             updateDateInView()
         }
         binding.etDate.setOnClickListener(this)
+        binding.tvAddImage.setOnClickListener(this)
     }
 
     override fun onDestroy() {
@@ -54,7 +66,63 @@ class AddHappyPlaceActivity : AppCompatActivity(), View.OnClickListener {
                     calendar.get(Calendar.DAY_OF_MONTH)
                 ).show()
             }
+            R.id.tv_add_image -> {
+                val pictureDialog = AlertDialog.Builder(this)
+                pictureDialog.setTitle("Select Action")
+
+                val pictureDialogItems =
+                    arrayOf("Select photo from Gallery", "Capture photo from camera")
+                pictureDialog.setItems(pictureDialogItems) { _, which ->
+                    when (which) {
+                        0 -> choosePhotoFromGallery()
+                        1 -> Toast.makeText(
+                            applicationContext, "Camera selection coming soon", Toast.LENGTH_SHORT
+                        ).show()
+                    }
+                }
+                pictureDialog.show()
+            }
         }
+    }
+
+    private fun choosePhotoFromGallery() {
+        Dexter.withContext(this).withPermissions(
+            android.Manifest.permission.READ_EXTERNAL_STORAGE,
+            android.Manifest.permission.WRITE_EXTERNAL_STORAGE
+        ).withListener(object : MultiplePermissionsListener {
+            override fun onPermissionsChecked(report: MultiplePermissionsReport?) {
+                if (report!!.areAllPermissionsGranted()) {
+                    Toast.makeText(
+                        this@AddHappyPlaceActivity,
+                        "Storage READ/WRITE permission are granted. Now you can select an image from GALLERY",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+            }
+
+            override fun onPermissionRationaleShouldBeShown(
+                permissions: MutableList<PermissionRequest>?, token: PermissionToken?
+            ) {
+                showRationalDialogForPermissions()
+            }
+        }).onSameThread().check()
+    }
+
+    private fun showRationalDialogForPermissions() {
+        AlertDialog.Builder(this)
+            .setMessage("It looks like you have turned off permission required for this feature. It can be enabled under Application Settings")
+            .setPositiveButton("GO TO SETTINGS") { _, _ ->
+                try {
+                    val intent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS)
+                    val uri = Uri.fromParts("package", packageName, null)
+                    intent.data = uri
+                    startActivity(intent)
+                } catch (e: ActivityNotFoundException) {
+                    e.printStackTrace()
+                }
+            }.setNegativeButton("Cancel") { dialog, _ ->
+                dialog.dismiss()
+            }.show()
     }
 
     private fun updateDateInView() {
