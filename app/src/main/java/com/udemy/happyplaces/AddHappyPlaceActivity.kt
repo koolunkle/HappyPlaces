48acccb4d6397e583d2169ac16388252fc5d751c
koolunkle <a43361904@gmail.com>
2023-02-04 16:07:11 +0900

    AddHappyPlaceActivity 추가

--- app/src/main/java/com/udemy/happyplaces/AddHappyPlaceActivity.kt
+++ app/src/main/java/com/udemy/happyplaces/AddHappyPlaceActivity.kt
@@ -1,11 +1,30 @@
 package com.udemy.happyplaces
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.udemy.happyplaces.databinding.ActivityAddHappyPlaceBinding
 
 class AddHappyPlaceActivity : AppCompatActivity() {
+
+    private var _binding: ActivityAddHappyPlaceBinding? = null
+
+    private val binding get() = _binding!!
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_add_happy_place)
+        _binding = ActivityAddHappyPlaceBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setSupportActionBar(binding.toolbarAddPlace)
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        binding.toolbarAddPlace.setNavigationOnClickListener {
+            onBackPressed()
+        }
     }
+
+    override fun onDestroy() {
+        _binding = null
+        super.onDestroy()
+    }
+
 }
--- app/src/main/java/com/udemy/happyplaces/MainActivity.kt
+++ app/src/main/java/com/udemy/happyplaces/MainActivity.kt
@@ -1,11 +1,30 @@
 package com.udemy.happyplaces
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.udemy.happyplaces.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private var _binding: ActivityMainBinding? = null
+
+    private val binding get() = _binding!!
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        _binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.fabAddHappyPlace.setOnClickListener {
+            val intent = Intent(this, AddHappyPlaceActivity::class.java)
+            startActivity(intent)
+        }
     }
+
+    override fun onDestroy() {
+        _binding = null
+        super.onDestroy()
+    }
+
 }
