cef2c32cc4f95c9873738e834356510a2ac07e63
koolunkle <a43361904@gmail.com>
2023-02-04 21:23:36 +0900

    로컬 DB 데이터 읽기

--- app/src/main/java/com/udemy/happyplaces/activities/MainActivity.kt
+++ app/src/main/java/com/udemy/happyplaces/activities/MainActivity.kt
@@ -2,8 +2,11 @@ package com.udemy.happyplaces.activities
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import com.udemy.happyplaces.database.DatabaseHandler
 import com.udemy.happyplaces.databinding.ActivityMainBinding
+import com.udemy.happyplaces.models.HappyPlaceModel
 
 class MainActivity : AppCompatActivity() {
 
@@ -19,6 +22,19 @@ class MainActivity : AppCompatActivity() {
         binding.fabAddHappyPlace.setOnClickListener {
             val intent = Intent(this, AddHappyPlaceActivity::class.java)
             startActivity(intent)
+        }
+        getHappyPlacesListFromLocalDatabase()
+    }
+
+    private fun getHappyPlacesListFromLocalDatabase() {
+        val databaseHandler = DatabaseHandler(this)
+        val getHappyPlaceList: ArrayList<HappyPlaceModel> = databaseHandler.getHappyPlaceList()
+
+        if (getHappyPlaceList.size > 0) {
+            for (i in getHappyPlaceList) {
+                Log.e("Title", i.title)
+                Log.e("Description", i.description)
+            }
         }
     }
 
